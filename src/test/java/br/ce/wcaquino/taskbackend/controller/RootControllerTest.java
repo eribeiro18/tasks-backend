765eb621fb9a93cccd3db8dc728237dfe3e784dc
Evandro Ribeiro <eribeiro18@gmail.com>
2024-04-10 20:41:23 -0300

    ajustes

--- src/test/java/br/ce/wcaquino/taskbackend/controller/RootControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/RootControllerTest.java
@@ -2,8 +2,11 @@ package br.ce.wcaquino.taskbackend.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class RootControllerTest {
 
 	@InjectMocks
