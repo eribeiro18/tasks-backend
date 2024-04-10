0a02bea48a7933ef8ba8f5c931b4bfc830ca9d44
Evandro Ribeiro <eribeiro18@gmail.com>
2024-04-10 20:23:27 -0300

    ajustes

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -3,6 +3,8 @@ package br.ce.wcaquino.taskbackend.utils;
 import java.time.LocalDate;
 
 public class DateUtils {
+	
+	private DateUtils() {}
 
 	public static boolean isEqualOrFutureDate(LocalDate date) {
 		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
